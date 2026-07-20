67ffedbe2be8a88cc07d62adb3c3403f5f268bb8
vineshkumar553 <vineshkumaraz1@gmail.com>
2026-07-20 16:22:14 +0500

    adding separate javascript file

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+
+        function showSidebar() {
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.style.right = "0";
+        }
+
+        function hideSidebar() {
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.style.right = "-230px";
+        }
+
+    </script>
